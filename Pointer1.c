e397ded8da6508231abb55687b843224ed1640a6
GwonYuBeen <eummapum97@naver.com>
2019-07-26 16:56:31 +0900

    Team1

---
+++ Pointer1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+
+char name [3][3][10];
+int score [3][3][4] = {0};
+float avg [3][3]={0};
+int i,j,k ;
+
+//ют ╥б
+for (i=0; i<3; i++){
+printf("%d ╧щ \n", i+1)
+for (j=0; j<3; j++) 
+
---
+++ arry2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+int a[4] = {4,3,2,1};
+printf("%d",*a);
+
+printf("\n");
+
+int b[2][2] = {4,3,2,1};
+printf("%d",b);
+
+printf("\n");
+
+
+
+printf("\n");
+
+int d[5] = {};
+printf("%d", d+1);
+
+}
